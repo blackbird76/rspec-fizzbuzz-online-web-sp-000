ed0eade10b3b8e060c498a8b783e329ab7fdc410
Megan McCullough <mmccullough206@gmail.com>
2019-03-14 17:48:50 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,11 @@
 require 'pry'
 def fizzbuzz(int)
-  if int % 3 == 0
-  binding.pry
-    "Fizz"
+  if int % 3 == 0 && int % 5 == 0  
+    "FizzBuzz"  
   elsif int % 5 == 0
     "Buzz"
-  elsif int % 3 == 0 && int % 5 == 0
-    "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
   else
     nil
   end
